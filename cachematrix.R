b8af8fa8887bf6157de0e581dee28be43eaca728
T J Zeeman <tjzeeman@xs4all.nl>
2015-06-21 19:14:14 +0200

    Implemented cached solve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Module for working with matrices and their inverse where inverted matrices 
+## are cached for performance reasons.
 
-## Write a short comment describing this function
+## Create a new instance of a matrix which can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solve a matrix such that if it was already solved (inverted) before a cached answer is returned instead of
+## doing all the calculations again. Any answer calculated will be cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
